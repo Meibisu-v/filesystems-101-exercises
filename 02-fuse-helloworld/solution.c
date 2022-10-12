b66e7c3d82440117388d894fbe053f1e63dede57
miokovichel <miokovich.e@phystech.edu>
2022-10-12 19:35:50 +0300

    fix output pid

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -28,26 +28,25 @@ fs_readdir(const char *path, void *data, fuse_fill_dir_t filler,
 static int
 fs_read(const char *path, char *buf, size_t size, off_t off,
         struct fuse_file_info *ffi)
-{
-	
+{	
 	// printf( "--> Trying to read %s, %lu, %lu\n", path, off, size );
 	if(strcmp(path, "/hello") != 0)
 		return -ENOENT;
 	(void) ffi;
-	size_t len;	
+	ssize_t len;	
 	struct fuse_context *fuse_pid = fuse_get_context();
-	sprintf(hello_str, "hello, %d\n", fuse_pid->pid);
+	sprintf(hello_str, "hello, %d\n%c", fuse_pid->pid, '\0');
 
 	len = strlen(hello_str);
 
-	if ((size_t)off < len) {
-		if ((size_t) off + size > len)
-			size = len - (size_t) off;
-		memcpy(buf, hello_str, size);
-	} else
-		size = 0;
+	if (off < len) {
+		if (off + (ssize_t)size > len)
+			size = len - off;
+	} //else
+		// size = 0;
 	// char *selected_text = hello_str;
 	// printf("size %lu, %s", size, selected_text);
+		memcpy(buf, hello_str, size);
 
 	return size;
 }
