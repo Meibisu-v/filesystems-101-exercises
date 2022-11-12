f6d946b1f924549a4ee2fb9238a2bfbd862c3517
miokovichel <miokovich.e@phystech.edu>
2022-11-12 13:31:07 +0300

    fix

--- 06-ext2-walk-path/solution.c
+++ 06-ext2-walk-path/solution.c
@@ -45,7 +45,7 @@ int dump_file(int img, const char *path, int out) {
     int inode_nr = EXT2_ROOT_INO;
     ret = get_inode_num_by_path(img, &inode_nr, &s_block, path_copy);
     if (ret < 0) {
-        assert(ret != -ENOTDIR);
+        assert(ret != -ENOENT);
         return ret;
     }
     struct ext2_inode inode;
