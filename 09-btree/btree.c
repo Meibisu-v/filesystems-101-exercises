7bf7f94dc035c98f3d70b1be1658b835772a0de0
miokovichel <miokovich.e@phystech.edu>
2022-11-20 21:28:06 +0300

    fix

--- 09-btree/btree.c
+++ 09-btree/btree.c
@@ -195,14 +195,16 @@ void btree_delete_key(struct btree* T, struct Node* node, int x) {
                     cur = cur->children[cur->n];
                 }
                 node->key[idx] = cur->key[cur->n - 1];
-                btree_delete_key(T, node->children[idx], node->key[idx]);
+                int val = node->key[idx];
+                btree_delete_key(T, node->children[idx], val);
             } else if (node->children[idx + 1]->n >= node->t) {
                 struct Node* cur = node->children[idx + 1];
                 while (!cur->leaf) {
                     cur = cur->children[0];
                 }
                 node->key[idx] = cur->key[0];
-                btree_delete_key(T, node->children[idx + 1], node->key[idx]);
+                int val = node->key[idx];
+                btree_delete_key(T, node->children[idx + 1], val);
             } else {
                 btree_merge_key(T, node, idx);
                 btree_delete_key(T, node->children[idx], x);
