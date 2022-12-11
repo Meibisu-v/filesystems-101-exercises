6c24b45cb181fa6dcdffa012d45e8b639a4089ec
miokovichel <miokovich.e@phystech.edu>
2022-12-11 15:56:00 +0300

    init

--- 12-grpc-prometheus/pkg/parhash/server.go
+++ 12-grpc-prometheus/pkg/parhash/server.go
@@ -2,10 +2,20 @@ package parhash
 
 import (
 	"context"
+	"net"
+	"time"
+	"sync"
+	"log"
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/sync/semaphore"
+	"google.golang.org/grpc"
+	
+
+	hashpb "fs101ex/pkg/gen/hashsvc"
+	parhashpb "fs101ex/pkg/gen/parhashsvc"
+	"fs101ex/pkg/workgroup"
 )
 
 type Config struct {
@@ -54,7 +64,15 @@ type Config struct {
 type Server struct {
 	conf Config
 
+	stop context.CancelFunc
+	l    net.Listener
+	wg   sync.WaitGroup
 	sem *semaphore.Weighted
+	currentBackend int
+	lock   sync.Mutex
+
+	nr_nr_requests prometheus.Counter
+	subquery_durations *prometheus.HistogramVec
 }
 
 func New(conf Config) *Server {
@@ -66,17 +84,93 @@ func New(conf Config) *Server {
 
 func (s *Server) Start(ctx context.Context) (err error) {
 	defer func() { err = errors.Wrap(err, "Start()") }()
+	s.currentBackend=0
+	ctx, s.stop = context.WithCancel(ctx)
 
-	/* implement me */
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
 
+	srv := grpc.NewServer()
+	// hashpb.RegisterHashSvcServer(srv, s)
+	parhashpb.RegisterParallelHashSvcServer(srv, s)
+
+	s.wg.Add(2)
+	go func() {
+		defer s.wg.Done()
+
+		srv.Serve(s.l)
+	}()
+	go func() {
+		defer s.wg.Done()
+
+		<-ctx.Done()
+		s.l.Close()
+	}()
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	return s.l.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+	s.stop()
+	s.wg.Wait()
+}
+
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashpb.ParHashReq) (resp *parhashpb.ParHashResp, err error) {
+	
+	// ctx := context.Background()
+	conn := make([]*grpc.ClientConn, len(s.conf.BackendAddrs))
+	for i := range conn {
+		conn[i], err = grpc.Dial(s.conf.BackendAddrs[i],
+			grpc.WithInsecure(), /* allow non-TLS connections */
+		)
+		if err != nil {
+			log.Fatalf("failed to connect to %q: %v", s.conf.BackendAddrs[i], err)
+		}
+		defer conn[i].Close()
+	}
+	client := make([]hashpb.HashSvcClient, len(s.conf.BackendAddrs))
+	for i := range client {
+		client[i] = hashpb.NewHashSvcClient(conn[i])
+	}
+	var (
+		wg     = workgroup.New(workgroup.Config{Sem: s.sem})
+		hashes = make([][]byte, len(req.Data))
+	)
+	s.nr_nr_requests.Inc()
+	for i := range req.Data {
+		qr := i
+		wg.Go(ctx, func(ctx context.Context) (err error) {
+			s.lock.Lock()
+			currentBackend := s.currentBackend
+			s.currentBackend += 1
+			if (s.currentBackend >= len(s.conf.BackendAddrs)) {
+				s.currentBackend =0
+			}
+			s.lock.Unlock()
+
+			time_s := time.Now()
+			resp, err := client[currentBackend].Hash(ctx, &hashpb.HashReq{Data: req.Data[qr]})
+			time_f := time.Since(time_s)
+			if err != nil {
+				return err
+			}
+
+			s.lock.Lock()
+			hashes[qr] = resp.Hash
+			s.lock.Unlock()
+			s.subquery_durations.With(prometheus.Labels{"backend" : s.conf.BackendAddrs[currentBackend]}).Observe(
+				float64(time_f))
+			return nil
+		})
+	}
+	if err := wg.Wait(); err != nil {
+		log.Fatalf("failed to hash files: %v", err)
+	}
+	return &parhashpb.ParHashResp{Hashes: hashes}, nil
 }
